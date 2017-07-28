ecc58f3b84c4816bc8b4456f76304220f661f855
gowisgreat1995 <gowisgreat1995@gmail.com>
2017-07-28 23:08:11 +0530

    change

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,11 @@
 var express = require('express');
+var url=require("url");
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     
-    if(req.query)
-        {
-             var time=new Date(req.query);
-    var utc=time.getTime();
-    var normal=months[time.getMonth()]+" "+time.getDate()+","+time.getFullYear();
-    
-    var item={"unix":utc,"natural":normal}
-    res.render('jsonShow',{data:JSON.stringify(item)});
-        }
-    else
-  res.render('index', { title: 'Timestamp Microservice' });
+  res.render('index', { title: req.url.pathname });
     
   
   
