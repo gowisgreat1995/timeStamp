29c16adc7768d1b7e4604b0cc73791a20ea95fb2
gowisgreat1995 <gowisgreat1995@gmail.com>
2017-07-28 23:40:42 +0530

    prototype

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var url=require("url").parse(url,true);
+console.log(url);
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
